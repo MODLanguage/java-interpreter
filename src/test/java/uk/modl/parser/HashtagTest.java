77f7cb27df9a2735cb1c98355d2a7291213791a9
Tony Walmsley <tony@aosd.co.uk>
2019-02-16 13:07:12 +0000

    Added a test for hashtags - there didn't seem to be any.

---
+++ src/test/java/uk/modl/parser/HashtagTest.java
@@ -0,0 +1,77 @@
+/*
+MIT License
+
+Copyright (c) 2018 NUM Technology Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package uk.modl.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import uk.modl.parser.printers.JsonPrinter;
+
+import java.io.IOException;
+
+/**
+ * @author twalmsley
+ * <p>
+ * I run the tests individually rather than in a loop to make it easier to see which ones fail.
+ * This way the early failures don't mask later ones.
+ */
+public class HashtagTest {
+    private final static String[][] expected = {{
+            // Normal NB array
+            "test=this is a #hashtag test\n" +
+                    "test2=#testing 123", "[ {\n" +
+            "  \"test\" : \"this is a #hashtag test\"\n" +
+            "}, {\n" +
+            "  \"test2\" : \"#testing 123\"\n" +
+            "} ]"
+    }
+    };
+
+    /**
+     * @throws IOException on test failure
+     */
+    @Test
+    public void test_00() throws IOException {
+        singleCase(expected[0]);
+    }
+
+    /**
+     * @param test The current test case
+     * @throws IOException on test failure
+     */
+    private void singleCase(final String[] test) throws IOException {
+        String input = test[0];
+        String expected = test[1];
+
+        System.out.println("Input : " + input);
+        System.out.println("Expected : " + expected);
+
+        RawModlObject rawModlObject = ModlObjectCreator.processModlParsed(input);
+        String output = JsonPrinter.printModl(rawModlObject);
+        System.out.println("Output : " + output);
+        Assert.assertEquals(expected
+                        .replace(" ", "")
+                        .replace("\n", "")
+                        .replace("\r", ""),
+                output
+                        .replace(" ", "")
+                        .replace("\n", "")
+                        .replace("\r", ""));
+    }
+}
