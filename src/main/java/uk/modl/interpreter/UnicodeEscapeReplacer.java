e6f716b3f0a7132474b0ae811cb0784a6e7dce5e
Tony Walmsley <tony@aosd.co.uk>
2019-10-16 14:09:35 +0100

    Slight refactoring of the unicode replacer.

--- src/main/java/uk/modl/interpreter/UnicodeEscapeReplacer.java
+++ src/main/java/uk/modl/interpreter/UnicodeEscapeReplacer.java
@@ -20,6 +20,14 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 package uk.modl.interpreter;
 
 class UnicodeEscapeReplacer {
+
+    private static final String BACKSLASH_U = "\\u";
+    private static final String TILDE_U = "~u";
+    private static final char TILDE = '~';
+    private static final char BACKSLASH = '\\';
+    private static final int ESCAPE_SEQUENCE_LENGHT = 6;
+    private static final int HEX = 16;
+
     /**
      * Convert explicit unicode escape sequences to unicode characters.
      * (recursive implementation)
@@ -28,50 +36,57 @@ class UnicodeEscapeReplacer {
      * @return the string with escape sequences converted to unicode characters.
      */
     static String convertUnicodeSequences(final String str) {
-        // Filter out cases with no escape sequences.
-        if (str == null) {
-            return null;
-        }
-
         int start = 0;
         String result = str;
-        while (true) {
-            final int backslashUIndex = result.indexOf("\\u", start);
-            final int tildeUIndex = result.indexOf("~u", start);
+        while (result != null) {
+            // We could have a backslash-u escape sequence or a ~u escape sequence
+            final int backslashUIndex = result.indexOf(BACKSLASH_U, start);
+            final int tildeUIndex = result.indexOf(TILDE_U, start);
 
             // Filter out cases with no escape sequences.
+            int unicodeStrIdx;
             if (tildeUIndex < 0 && backslashUIndex < 0) {
                 break;
-            }
-
-            int unicodeStrIdx;
-            if (tildeUIndex < 0) {
-                unicodeStrIdx = backslashUIndex;
+            } else if (tildeUIndex < 0) {
+                unicodeStrIdx = backslashUIndex;// No ~? Must be backslash
             } else if (backslashUIndex < 0) {
-                unicodeStrIdx = tildeUIndex;
+                unicodeStrIdx = tildeUIndex;// No backslash? Must be ~
             } else {
+                // Pick the first escaped character and proceed with that one.
                 unicodeStrIdx = Math.min(backslashUIndex, tildeUIndex);
             }
 
-            if (unicodeStrIdx + 6 > result.length()) {
+            // Do we have a long enough value? Break out if not.
+            if (unicodeStrIdx + ESCAPE_SEQUENCE_LENGHT > result.length()) {
                 break;
             }
 
+            // Next time round the loop we start searching after the current escape sequence.
             start = unicodeStrIdx + 1;
 
-            if (unicodeStrIdx > 0 && result.charAt(unicodeStrIdx - 1) == '~') {
-                continue;
-            }
-            if (unicodeStrIdx > 0 && result.charAt(unicodeStrIdx - 1) == '\\') {
+            // If the escape sequence is itself escaped then don't replace it
+            if (unicodeStrIdx > 0 && (result.charAt(unicodeStrIdx - 1) == TILDE || result.charAt(unicodeStrIdx - 1) == BACKSLASH)) {
                 continue;
             }
 
-            final int codePoint = Integer.parseInt(result.substring(unicodeStrIdx + 2, unicodeStrIdx + 6), 16);
-            final char uni_val = (char) codePoint;
-            final String left = result.substring(0, unicodeStrIdx);
-            final String right = result.substring(unicodeStrIdx + 6);
-            result = left + uni_val + right;
+            // Get the codepoint value and replace the escape sequence
+            final int codePoint = Integer.parseInt(result.substring(unicodeStrIdx + 2, unicodeStrIdx + ESCAPE_SEQUENCE_LENGHT), HEX);
+            result = replace(result, (char) codePoint, unicodeStrIdx);
         }
         return result;
+    }
+
+    /**
+     * Replace a unicode value in a String
+     *
+     * @param s             the String with the unicode value to be replaced.
+     * @param value         the replacement character
+     * @param unicodeStrIdx the index of the unicode escape sequence
+     * @return a String with the unicode escape sequence replaced by the replacement character
+     */
+    private static String replace(final String s, final char value, final int unicodeStrIdx) {
+        final String left = s.substring(0, unicodeStrIdx);
+        final String right = s.substring(unicodeStrIdx + ESCAPE_SEQUENCE_LENGHT);
+        return left + value + right;
     }
 }
