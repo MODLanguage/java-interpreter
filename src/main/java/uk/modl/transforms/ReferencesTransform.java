a2a9c08a9a704affce6f1027fc2222186d735bbe
Tony Walmsley <tony@aosd.co.uk>
2020-02-17 11:34:37 +0000

    Checkpoint - ReferencesTransform.

--- src/main/java/uk/modl/transforms/ReferencesTransform.java
+++ src/main/java/uk/modl/transforms/ReferencesTransform.java
@@ -141,11 +141,6 @@ public class ReferencesTransform implements Function1<Modl, Modl> {
          * @return the updated copy of the Modl object
          */
         public Modl replace(final Modl modl) {
-            if (pairKeysWithReferences.size() == 0) {
-                // No references so nothing to do
-                return modl;
-            }
-
             final List<Structure> list = List.ofAll(modl.structures.map(this::replace));
             return new Modl(list);
         }
@@ -225,40 +220,247 @@ public class ReferencesTransform implements Function1<Modl, Modl> {
             return arr;
         }
 
+        /**
+         * Replace if necessary
+         *
+         * @param conditional an ArrayConditional
+         * @return an ArrayConditional
+         */
         private ArrayConditional replace(final ArrayConditional conditional) {
-            // TODO
-            conditional.tests.map(test -> {
-                test.conditions.map(cond -> {
-                    if (cond._1 instanceof Condition) {
-                        final String lhs = ((Condition) cond._1).lhs;// TODO
-                        final String newLhs = replace(lhs);
-                        final List<ValueItem> values = ((Condition) cond._1).values;// TODO
-                    } else if (cond._1 instanceof ConditionGroup) {
 
+            final List<ConditionTest> newConditionTests = conditional.tests.map(this::replace);
+
+            final List<ArrayConditionalReturn> arrayConditionalReturns = conditional.returns.map(ret -> {
+                final List<ArrayItem> arrayItems = ret.items.map(ai -> {
+                    if (ai instanceof ArrayConditional) {
+                        return replace((ArrayConditional) ai);
+                    } else if (ai instanceof uk.modl.model.Map) {
+                        return replace((uk.modl.model.Map) ai);
+                    } else if (ai instanceof Pair) {
+                        return replace((Pair) ai);
+                    } else if (ai instanceof Array) {
+                        return replace((Array) ai);
+                    } else if (ai instanceof Primitive) {
+                        return (Primitive) replace((Primitive) ai);
+                    } else {
+                        throw new InterpreterError("Unknown ArrayItem type: " + ai.getClass());
                     }
                 });
-            });// TODO
 
-            conditional.returns.map(ret -> {
+                if (!ret.items.equals(arrayItems)) {
+                    return new ArrayConditionalReturn(arrayItems);
+                }
+                return ret;
+            });
 
-            });// TODO
-
-            // TODO return new Conditional if there are changes
+            if (!newConditionTests.equals(conditional.tests) || !arrayConditionalReturns.equals(conditional.returns)) {
+                return new ArrayConditional(newConditionTests, arrayConditionalReturns);
+            }
             return conditional;
         }
 
-        private String replace(final String s) {
-            // TODO
-            return s;
+        /**
+         * Replace if necessary
+         *
+         * @param test a ConditionTest
+         * @return a ConditionTest
+         */
+        private ConditionTest replace(final ConditionTest test) {
+            final List<Tuple2<ConditionOrConditionGroupInterface, String>> newConditions = test.conditions.map(cond -> {
+                if (cond._1 instanceof Condition) {
+                    final Condition condition = (Condition) cond._1;
+                    return cond.update1(replace(condition));
+                } else if (cond._1 instanceof ConditionGroup) {
+                    final ConditionGroup cg = (ConditionGroup) cond._1;
+                    return cond.update1(replace(cg));
+                }
+                return cond;
+            });
+
+            if (!newConditions.equals(test.conditions)) {
+                return new ConditionTest(newConditions);
+            }
+            return test;
         }
 
-        private Structure replace(final uk.modl.model.TopLevelConditional tlc) {
-            // TODO
+        /**
+         * Replace if necessary
+         *
+         * @param cg a ConditionGroup
+         * @return a ConditionGroup
+         */
+        private ConditionGroup replace(final ConditionGroup cg) {
+            final List<Tuple2<ConditionTest, String>> tests = cg.subConditionList.map(tuple -> tuple.update1(replace(tuple._1)));
+            if (!tests.equals(cg.subConditionList)) {
+                return new ConditionGroup(tests);
+            }
+            return cg;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param condition a Condition
+         * @return a Condition
+         */
+        private Condition replace(final Condition condition) {
+            final String lhs = condition.lhs;
+            final String newLhs = (lhs == null) ? null : replace(lhs);
+            final List<ValueItem> values = condition.values;
+            final List<ValueItem> valueItems = values.map(this::replace);
+
+            if (!valueItems.equals(values)) {
+                return new Condition(newLhs, condition.op, valueItems);
+            }
+            return condition;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param v a ValueConditional
+         * @return a ValueConditional
+         */
+        private ValueConditional replace(final ValueConditional v) {
+            final List<ConditionTest> tests = v.tests.map(this::replace);
+            final List<ValueConditionalReturn> returns = v.returns.map(this::replace);
+            if (!tests.equals(v.tests) || !returns.equals(v.returns)) {
+                return new ValueConditional(tests, returns);
+            }
+            return v;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param ret a ValueConditionalReturn
+         * @return a ValueConditionalReturn
+         */
+        private ValueConditionalReturn replace(final ValueConditionalReturn ret) {
+            final List<ValueItem> items = ret.items.map(this::replace);
+            if (!items.equals(ret.items)) {
+                return new ValueConditionalReturn(items);
+            }
+            return ret;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param v a ValueItem
+         * @return a ValueItem
+         */
+        private ValueItem replace(final ValueItem v) {
+            if (v instanceof ValueConditional) {
+                return replace((ValueConditional) v);
+            } else if (v instanceof uk.modl.model.Map) {
+                return replace((uk.modl.model.Map) v);
+            } else if (v instanceof Pair) {
+                return replace((Pair) v);
+            } else if (v instanceof Array) {
+                return replace((Array) v);
+            } else if (v instanceof StringPrimitive) {
+                final String s = replace(((StringPrimitive) v).value);
+                if (!s.equals(((StringPrimitive) v).value)) {
+                    return new StringPrimitive(s);
+                }
+            }
+            return v;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param s a String
+         * @return a String
+         */
+        private String replace(final String s) {
+            final Map<ReferenceType, List<String>> groupedByType = getReferenceGroups(s);
+
+            // Process the OBJECT_INDEX_REF entries
+            String result = groupedByType.get(ReferenceType.OBJECT_INDEX_REF)
+                    .map(this::indexToReferencedObject)
+                    .map(replaceAllObjectIndexRefsInString(s))
+                    .getOrElse(s);
+
+            // Process simple String references, e.g. %val% etc.
+            result = groupedByType.get(ReferenceType.SIMPLE_REF)
+                    .map(this::keyToReferencedObject)
+                    .map(replaceAllSimpleRefsInString(result))
+                    .getOrElse(result);
+            return result;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param tlc a TopLevelConditional
+         * @return a TopLevelConditional
+         */
+        private Structure replace(final TopLevelConditional tlc) {
+            final List<ConditionTest> tests = tlc.tests.map(this::replace);
+            final List<TopLevelConditionalReturn> returns = tlc.returns.map(this::replace);
+            if (!tests.equals(tlc.tests) || !returns.equals(tlc.returns)) {
+                return new TopLevelConditional(tests, returns);
+            }
             return tlc;
         }
 
+        /**
+         * Replace if necessary
+         *
+         * @param tlcr a TopLevelConditionalReturn
+         * @return a TopLevelConditionalReturn
+         */
+        private TopLevelConditionalReturn replace(final TopLevelConditionalReturn tlcr) {
+            final List<Structure> structures = tlcr.structures.map(this::replace);
+            if (!structures.equals(tlcr.structures)) {
+                return new TopLevelConditionalReturn(structures);
+            }
+            return tlcr;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param mi a MapConditional
+         * @return a MapConditional
+         */
         private MapConditional replace(final MapConditional mi) {
-            // TODO
+            final List<ConditionTest> tests = mi.tests.map(this::replace);
+            final List<MapConditionalReturn> returns = mi.returns.map(this::replace);
+            if (!tests.equals(mi.tests) || !returns.equals(mi.returns)) {
+                return new MapConditional(tests, returns);
+            }
+            return mi;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param mcr a MapConditionalReturn
+         * @return a MapConditionalReturn
+         */
+        private MapConditionalReturn replace(final MapConditionalReturn mcr) {
+            final List<MapItem> mapItems = mcr.items.map(this::replace);
+            if (!mapItems.equals(mcr.items)) {
+                return new MapConditionalReturn(mapItems);
+            }
+            return mcr;
+        }
+
+        /**
+         * Replace if necessary
+         *
+         * @param mi a MapItem
+         * @return a MapItem
+         */
+        private MapItem replace(final MapItem mi) {
+            if (mi instanceof MapConditional) {
+                return replace((MapConditional) mi);
+            } else if (mi instanceof Pair) {
+                return replace((Pair) mi);
+            }
             return mi;
         }
 
@@ -269,8 +471,13 @@ public class ReferencesTransform implements Function1<Modl, Modl> {
          * @return a Pair with the references resolved
          */
         private Pair replace(final Pair p) {
-            return pairKeysWithReferences.get(p.key)
-                    .getOrElse(p);
+            if (p.value instanceof ValueConditional) {
+                final ValueConditional vc = replace((ValueConditional) p.value);
+                return new Pair(p.key, vc);
+            } else {
+                return pairKeysWithReferences.get(p.key)
+                        .getOrElse(p);
+            }
         }
 
         /**
@@ -279,18 +486,8 @@ public class ReferencesTransform implements Function1<Modl, Modl> {
         public void resolve() {
             pairKeysWithReferences = HashMap.ofEntries(
                     pairKeysWithReferences.map(tuple2 -> {
-                        final String stringWithRefs = tuple2._2.value.toString();
 
-                        final Matcher matcher = referencePattern.matcher(stringWithRefs);
-
-                        // Gather the match groups into a list of references
-                        List<String> groups = List.empty();
-                        while (matcher.find()) {
-                            groups = groups.append(matcher.group());
-                        }
-
-                        // Partition the references by reference type
-                        final Map<ReferenceType, List<String>> groupedByType = groups.groupBy(ReferencesMutator::refToRefType);
+                        final Map<ReferenceType, List<String>> groupedByType = getReferenceGroups(tuple2._2.value.toString());
 
                         // Process the OBJECT_INDEX_REF entries
                         Tuple2<String, Pair> result = groupedByType.get(ReferenceType.OBJECT_INDEX_REF)
@@ -313,6 +510,25 @@ public class ReferencesTransform implements Function1<Modl, Modl> {
         }
 
         /**
+         * Find references in a String
+         *
+         * @param stringWithRefs a String
+         * @return a Map of references by ReferenceType
+         */
+        private Map<ReferenceType, List<String>> getReferenceGroups(final String stringWithRefs) {
+            final Matcher matcher = referencePattern.matcher(stringWithRefs);
+
+            // Gather the match groups into a list of references
+            List<String> groups = List.empty();
+            while (matcher.find()) {
+                groups = groups.append(matcher.group());
+            }
+
+            // Partition the references by reference type
+            return groups.groupBy(ReferencesMutator::refToRefType);
+        }
+
+        /**
          * Return a function to fold a list of replacement values into a value with references, i.e. replace references with their actual values.
          *
          * @param tuple2 a Tuple2 of a String key and a Pair with references
@@ -325,11 +541,31 @@ public class ReferencesTransform implements Function1<Modl, Modl> {
         /**
          * Return a function to fold a list of replacement values into a value with references, i.e. replace references with their actual values.
          *
+         * @param s a String with references
+         * @return a function to convert the String to a String with references replaced
+         */
+        private Function<List<Tuple4<String, String, Integer, Option<ArrayItem>>>, String> replaceAllObjectIndexRefsInString(final String s) {
+            return refTuples -> refTuples.foldLeft(s, replaceObjectIndexRefInString());
+        }
+
+        /**
+         * Return a function to fold a list of replacement values into a value with references, i.e. replace references with their actual values.
+         *
          * @param tuple2 a Tuple2 of a String key and a Pair with references
          * @return a function to convert the Pair in the supplied tuple to a Pair with references replaced
          */
         private Function<List<Tuple3<String, String, Option<Pair>>>, Tuple2<String, Pair>> replaceAllSimpleRefs(final Tuple2<String, Pair> tuple2) {
             return refTuples -> refTuples.foldLeft(tuple2, replaceSimpleRefInPair());
+        }
+
+        /**
+         * Return a function to fold a list of replacement values into a value with references, i.e. replace references with their actual values.
+         *
+         * @param s a String with references
+         * @return a function to convert the String to a String with references replaced
+         */
+        private Function<List<Tuple3<String, String, Option<Pair>>>, String> replaceAllSimpleRefsInString(final String s) {
+            return refTuples -> refTuples.foldLeft(s, replaceSimpleRefInString());
         }
 
         /**
